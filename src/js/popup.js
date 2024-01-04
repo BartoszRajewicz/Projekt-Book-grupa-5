b298a9e7b718d694e8b9749194020c40d74d6557
KZapal <kamil.zapal@o2.pl>
2024-01-04 19:30:52 +0100

    my changes

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,58 +1,58 @@
-// import axios from 'axios';
-//a
+// // import axios from 'axios';
+// //a
 
-document.addEventListener(`DOMContentLoaded`, init);
+// document.addEventListener(`DOMContentLoaded`, init);
 
-const openBtn = document.querySelector(`.book-card`);
-const popup = document.querySelector(`.popup`);
-const closeBtn = document.querySelector(`.popup__close`);
-const bookInfo = document.querySelector(`.book`);
+// const openBtn = document.querySelector(`.book-card`);
+// const popup = document.querySelector(`.popup`);
+// const closeBtn = document.querySelector(`.popup__close`);
+// const bookInfo = document.querySelector(`.book`);
 
-// async function getInfoAbouBook(bookId) {
-//   try {
-//     const response = await axios.get('https://books-backend.p.goit.global/books/${bookId}');
-//     return response.data;
-//   } catch (err) {
-//     console.log(err);
-//   }
+// // async function getInfoAbouBook(bookId) {
+// //   try {
+// //     const response = await axios.get('https://books-backend.p.goit.global/books/${bookId}');
+// //     return response.data;
+// //   } catch (err) {
+// //     console.log(err);
+// //   }
+// // }
+
+// export default function init() {
+//   openBtn.addEventListener(`click`, () => {
+//     // let src = event.target.src;
+//     // let bookId = src.substring(src.length - 24);
+//     // getInfoAbouBook(response.data._id)
+//     //   .then(response => {
+//     //     bookInfo.innerHTML = `<div class="popup__flex book-info book">
+//     // //         <img
+//     // //           class="book__img"
+//     // //           src="${response.data.book_image}"
+//     // //           alt="${response.data.title}"
+//     // //         />
+//     // //         <div class="book-info__flex">
+//     // //           <h4 class="book__title">${response.data.title}</h4>
+//     // //           <p class="book__author">${response.data.author}</p>
+//     // //           <p class="book__description">
+//     // //             ${response.data.description}
+//     // //           </p>
+//     // //           <div class="trading-platform-icons">Miejsce na ikonki</div>
+//     // //         </div>
+//     // //       </div>`;
+//     //   })
+//     //   .catch(err => console.log(err));
+//     popup.style.display = `block`;
+//   });
+//   closeBtn.addEventListener(`click`, () => {
+//     popup.style.display = `none`;
+//   });
+//   document.addEventListener(`keydown`, event => {
+//     if (event.key === `Escape`) {
+//       popup.style.display = `none`;
+//     }
+//   });
+//   window.addEventListener(`click`, event => {
+//     if (event.target == popup) {
+//       popup.style.display = `none`;
+//     }
+//   });
 // }
-
-export default function init() {
-  openBtn.addEventListener(`click`, () => {
-    // let src = event.target.src;
-    // let bookId = src.substring(src.length - 24);
-    // getInfoAbouBook(response.data._id)
-    //   .then(response => {
-    //     bookInfo.innerHTML = `<div class="popup__flex book-info book">
-    // //         <img
-    // //           class="book__img"
-    // //           src="${response.data.book_image}"
-    // //           alt="${response.data.title}"
-    // //         />
-    // //         <div class="book-info__flex">
-    // //           <h4 class="book__title">${response.data.title}</h4>
-    // //           <p class="book__author">${response.data.author}</p>
-    // //           <p class="book__description">
-    // //             ${response.data.description}
-    // //           </p>
-    // //           <div class="trading-platform-icons">Miejsce na ikonki</div>
-    // //         </div>
-    // //       </div>`;
-    //   })
-    //   .catch(err => console.log(err));
-    popup.style.display = `block`;
-  });
-  closeBtn.addEventListener(`click`, () => {
-    popup.style.display = `none`;
-  });
-  document.addEventListener(`keydown`, event => {
-    if (event.key === `Escape`) {
-      popup.style.display = `none`;
-    }
-  });
-  window.addEventListener(`click`, event => {
-    if (event.target == popup) {
-      popup.style.display = `none`;
-    }
-  });
-}
