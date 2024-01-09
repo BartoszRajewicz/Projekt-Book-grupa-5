b31c9505ae9587f96816e59bcbc7818d37fae919
Mateusz Świetlik <swiecium@gmail.com>
2024-01-09 20:48:47 +0100

    adjustmentsUpdate

--- src/shopping.js
+++ src/shopping.js
@@ -2,7 +2,6 @@ import './js/shopping-list';
 import './js/charities';
 import './js/homeCategories';
 import './js/charitiesExport';
-import './js/scroll-up-button';
 import './js/shop-pagination';
 import './js/dark-mode-switch';
 import './js/shopOnClickDeleteBtn';
