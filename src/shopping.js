08d48929550fcb399aabcdc82ca0bc670673eff9
romanhalaichuk <vi2virbien@gmail.com>
2024-01-13 23:23:53 +0100

    sfdb

--- src/shopping.js
+++ src/shopping.js
@@ -1,6 +1,5 @@
 import './js/shopping-list';
 import './js/charities';
-import './js/homeCategories';
+
 import './js/charitiesExport';
 import './js/dark-mode-switch';
-import './js/shop-delete-btn';
